f2de174e344c008b455de1e66c6d87f7129c0544
bin.zeng <yy282371715@sina.cn>
2023-10-09 09:33:45 +0800

    feat: 初始化仓库

---
+++ EventEmitter.js
@@ -0,0 +1,121 @@
+/**
+ * @author Bin.zeng
+ * create by 2023/10/08
+ * */
+
+/**
+ * 验证是否是有效的侦听者函数
+ * */
+function isValidListener(listener) {
+  return typeof listener === 'function'
+}
+
+/**
+ * 为指定事件添加侦听器
+ * */
+function addListener(emitter, event, fn, context, once) {
+  if (!isValidListener(fn))
+    throw  new TypeError("listener 必须是一个函数！")
+
+  const listener = {
+    fn,
+    context,
+    once,
+  }
+
+  const listeners = emitter._events[event] = (emitter._events[event] || [])
+
+  listeners.push(listener)
+
+  return emitter
+}
+
+/**
+ * EventEmitter 事件发射器
+ *
+ * 继承自 EventEmitter 类，类的实例即是被观察者，可实现观察者模式。
+ *
+ * 全局创建 EventEmitter 实例，用作事件调度中心，可实现发布/订阅模式。
+ * */
+class EventEmitter {
+
+  constructor() {
+    this._events = {}
+    // 订阅事件
+    this.subscribe = this.on
+    // 订阅事件，该事件只能被执行一次
+    this.subscribeOnce = this.once
+    // 发布事件
+    this.publish = this.emit
+    // 取消订阅事件
+    this.unsubscribe = this.off
+  }
+
+  /**
+   * 侦听事件
+   * @param  {String} event 事件名称
+   * @param  {Function} listener 侦听器函数
+   * @param  {*} context 调用侦听器的上下文对象
+   * @return {EventEmitter} `this`
+   * */
+  on(event, listener, context) {
+    return addListener(this, event, listener, context || this, false)
+  }
+
+  /**
+   * 侦听事件，该事件只能被执行一次
+   * */
+  once(event, listener, context) {
+    return addListener(this, event, listener, context || this, true)
+  }
+
+  /**
+   * 派发事件
+   * @param  {String} event 事件名称
+   * @param  {*} arguments 传入侦听器函数的参数
+   * @return {EventEmitter} `this`
+   * */
+  emit(event) {
+    const listeners = this._events[event]
+    if (listeners) {
+      const len = arguments.length
+      const args = new Array(len - 1)
+
+      for (let j = 1; j < len; j++) {
+        args[j - 1] = arguments[j]
+      }
+
+      listeners.forEach(listener => {
+        if (listener.once)
+          this.off(event, listener)
+
+        listener.fn.apply(listener.context, args)
+      })
+    }
+
+    return this
+  }
+
+  /**
+   * 移除事件
+   * */
+  off(event, listener) {
+    const listeners = this._events[event]
+    if (!listeners) return
+
+    if (!listener) {
+      // 移除所有事件侦听器
+      delete this._events[event]
+    } else {
+      for (let l = listeners.length - 1; l >= 0; l--) {
+        if (listeners[l].fn === listener) {
+          listeners.splice(l, 1)
+        }
+      }
+    }
+
+    return this
+  }
+}
+
+export default EventEmitter
---
+++ README.md
@@ -0,0 +1,5 @@
+# EventEmitter 事件发射器
+
+> 继承自 EventEmitter 类，类的实例即是被观察者，可实现观察者模式。
+>
+> 全局创建 EventEmitter 实例，用作事件调度中心，可实现发布/订阅模式。
